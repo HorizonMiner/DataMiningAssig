fc88f23fcea85303cd2c61b4669367fcca55bca9
Ng Chen Hon <NgChenHon@Ngs-MacBook-Pro.local>
2016-12-27 13:44:21 +0800

    change loading method from zip to load csv

--- setup.R
+++ setup.R
@@ -1,6 +1,7 @@
 loadDataSetFromZip = function()
 {
-  dataset = read.csv(unz("dataset/dataset.zip", "uci-news-aggregator.csv"))
+  # dataset = read.csv(unz("dataset/dataset.zip", "uci-news-aggregator.csv"))
+  dataset = read.csv("dataset/movie_metadata.csv")
   dataset
 }
 
