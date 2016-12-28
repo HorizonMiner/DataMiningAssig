86331b085682d981129f69c7cb2ee03cff20d5ca
Ng Chen Hon <NgChenHon@Ngs-MacBook-Pro.local>
2016-12-29 00:15:57 +0800

    Added some functions:

--- Part1/QuestionA.R
+++ Part1/QuestionA.R
@@ -1,3 +1,26 @@
+source(file="setup.R")
+source(file="function.R")
+
+# load dataset
+print("loading dataset...")
+loadDataSetFromZip()
+
+# Number of rows.
+print("number of rows : ")
 dim(dataset)
+
+# Names of variables
+print("Names of variables in this dataset : ")
 names(dataset)
-summary(dataset)
+
+# Number of Empty Cells
+print("Number of empty cells in this dataset : ")
+numEmptyCellsInDataFrame(dataset)
+
+# Number of Weird HTML Tags
+print("Number of Weird HTML Tags : ")
+numOfHTMLTag(dataset)
+
+# Number of NA
+print("Number of NA in this dataset : ")
+numNACellsInDataFrame(dataset)
