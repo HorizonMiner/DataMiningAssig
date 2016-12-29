9d77807f3b9d8e7c7af3baf14a0a17bea73f738d
JohnKhor <0jetpistol0@gmail.com>
2016-12-30 03:26:59 +0800

    Add second insight

--- Part1/QuestionB.R
+++ Part1/QuestionB.R
@@ -11,3 +11,24 @@ print("Plot of facebook likes agaisnt year:")
 plot(x = dataset$title_year, y = dataset$movie_facebook_likes, xlab = "Year", ylab = "Facebook Likes", main = "Facebook Likes against Year", type = "l")
 # This plot shows that older movies clearly have less facebook likes
 # and recent movies have the most facebook likes.
+
+## Second insight
+
+# Mean of standard deviation of IMDB score with respect to director
+print("Mean of standard deviation of IMDB score with respect to director:")
+
+# Step 1: Create empty data frame with two columns
+directorWithSD <- data.frame(director = character(), standard_deviation = numeric())
+
+# Step 2: Calculate standard deviation of IMDB score for each director
+for (director in unique(dataset$director_name)) {
+  standardDeviation <- sd(dataset$imdb_score[dataset$director_name == director])
+  
+  # Append to data frame
+  directorWithSD <- rbind(directorWithSD, data.frame(director = director, standard_deviation = standardDeviation))
+}
+
+# Step 3: Calculate mean of standard deviation column
+mean(directorWithSD$standard_deviation, na.rm = TRUE) # yields 0.636415
+# This shows that directors are consistent since their standard deviation of 
+# IMDB score on average is not more than 1.
