7f87d8eb760f1f2233cf49d213f5d4841bf0893c
JohnKhor <0jetpistol0@gmail.com>
2016-12-30 03:25:39 +0800

    Remove function.R and setup.R

--- Part1/setup.R
+++
@@ -1,8 +0,0 @@
-loadDataSetFromZip = function()
-{
-  # dataset = read.csv(unz("dataset/dataset.zip", "uci-news-aggregator.csv"))
-  dataset = read.csv("dataset/movie_metadata.csv")
-  dataset
-}
-
-dataset = loadDataSetFromZip()
