3fe2970ed96743112ef04a66f7ed5474f3245a65
chinann6213 <chinann6213@gmail.com>
2017-01-12 21:00:16 +0800

    Modified goods.csv

--- Part2/Question2.R
+++ Part2/Question2.R
@@ -2,7 +2,7 @@
 
 ## Read Dataset
 # Read 1000-out1.csv
-receiptSparse <- read.csv("1000-out1.csv", stringsAsFactors = F, header = F)
+receiptSparse <- read.csv("1000-out1.csv", stringsAsFactors = F, header = F) # R will read the columns incorrectly
 dim(receiptSparse) # [1] 1048    7
 
 # Read 1000-out2.csv
@@ -22,4 +22,4 @@ dim(receiptSparse) # [1] 1000    9
 colnames(receiptBinary) <- c("ReceiptIndex", "ChocolateCake", "LemonCake", "CasinoCake", "OperaCake", "StrawberryCake", "TruffleCake", "ChocolateEclair", "CoffeeEclair", "VanillaEclair", "NapoleonCake", "AlmondTart", "ApplePie", "AppleTart", "ApricotTart", "BerryTart", "BlackberryTart", "BlueberryTart", "ChocolateTart", "CherryTart", "LemonTart", "PecanTart", "GanacheCookie", "GongolaisCookie", "RaspberryCookie", "LemonCookie", "ChocolateMeringue", "VanillaMeringue", "MarzipanCookie", "TuileCookie", "WalnutCookie", "AlmondCroissant", "AppleCroissant", "ApricotCroissant", "CheeseCroissant", "ChocolateCroissant", "ApricotDanish", "AppleDanish", "AlmondTwist", "AlmondBearClaw", "BlueberryDanish", "LemonLemonade", "RaspberryLemonade", "OrangeJuie", "GreenTea", "BottledWater", "HotCoffee", "ChocolateCoffee", "VanillaFrappuccino", "CherrySoda", "SingleEspresso")
 
 # Add headers to columns in dataset.
-colnames(itemsTable) <- c("ReceiptIndex", "ItemIndex", "ItemQuantity")
+colnames(itemsTable) <- c("ReceiptIndex", "ItemQuantity", "ItemIndex")
