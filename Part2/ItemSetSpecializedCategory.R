adf8a1f4c4de6c5f8f8e135fda32192f5af58e5d
MutatedBread <chenhon199@yahoo.com>
2017-01-13 01:18:20 +0800

    push

--- Part2/ItemSetSpecializedCategory.R
+++ Part2/ItemSetSpecializedCategory.R
@@ -7,24 +7,24 @@ getUniqueFoodCategory = function()
 
 createCategoryTable = function()
 {
-  categoryReceipt = data.frame(receiptId = seq(1:1000))
+  categoryReceipt = data.frame(receiptId = seq(1:1000), stringsAsFactors = TRUE)
   
-  categoryReceipt$Eclair = rep(FALSE, 1000)
-  categoryReceipt$Tart = rep(FALSE, 1000)
-  categoryReceipt$Coffee = rep(FALSE, 1000)
-  categoryReceipt$Water = rep(FALSE, 1000)
-  categoryReceipt$Cake = rep(FALSE, 1000)
-  categoryReceipt$Danish = rep(FALSE, 1000)
-  categoryReceipt$Cookie = rep(FALSE, 1000)
-  categoryReceipt$Croissant = rep(FALSE, 1000)
-  categoryReceipt$Soda = rep(FALSE, 1000)
-  categoryReceipt$Lemonade = rep(FALSE, 1000)
-  categoryReceipt$Tea = rep(FALSE, 1000)
-  categoryReceipt$Meringue = rep(FALSE, 1000)
-  categoryReceipt$Pie = rep(FALSE, 1000)
-  categoryReceipt$Twist = rep(FALSE, 1000)
-  categoryReceipt$BearClaw = rep(FALSE, 1000)
-  categoryReceipt$OrangeJuie = rep(FALSE, 1000)
+  categoryReceipt$Eclair = rep("No", 1000)
+  categoryReceipt$Tart = rep("No", 1000)
+  categoryReceipt$Coffee = rep("No", 1000)
+  categoryReceipt$Water = rep("No", 1000)
+  categoryReceipt$Cake = rep("No", 1000)
+  categoryReceipt$Danish = rep("No", 1000)
+  categoryReceipt$Cookie = rep("No", 1000)
+  categoryReceipt$Croissant = rep("No", 1000)
+  categoryReceipt$Soda = rep("No", 1000)
+  categoryReceipt$Lemonade = rep("No", 1000)
+  categoryReceipt$Tea = rep("No", 1000)
+  categoryReceipt$Meringue = rep("No", 1000)
+  categoryReceipt$Pie = rep("No", 1000)
+  categoryReceipt$Twist = rep("No", 1000)
+  categoryReceipt$BearClaw = rep("No", 1000)
+  categoryReceipt$OrangeJuie = rep("No", 1000)
   
   categoryReceipt
 }
@@ -34,7 +34,7 @@ assignPurchasedItems = function()
   for(i in 1:nrow(itemsTable))
   {
     row = itemsTable[i,]
-    categoryReceipt[row$ReceiptIndex, match(row$FoodCategory, uniqueCategory) + 1] = TRUE
+    categoryReceipt[row$ReceiptIndex, match(row$FoodCategory, uniqueCategory) + 1] = "Yes"
   }
   categoryReceipt
 }
@@ -44,3 +44,9 @@ assignPurchasedItems = function()
 uniqueCategory = getUniqueFoodCategory()
 categoryReceipt = createCategoryTable()
 categoryReceipt = assignPurchasedItems()
+categoryReceipt = categoryReceipt[, 2:17]
+
+for(i in 1:ncol(categoryReceipt))
+{
+  categoryReceipt[, i] = as.factor(categoryReceipt[, i])
+}
